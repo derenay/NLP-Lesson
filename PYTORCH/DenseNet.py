5c0d2442ab1326a7976fee88b9bfbcf3d23ebb60
derenay <erenayarsal@hotmail.com>
2025-02-09 15:21:10 +0300

    DenseNet

---
+++ PYTORCH/DenseNet.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+
+
+# Dense layer
+
+class DenseLayer(nn.Module):
+    def __init__(self, in_channels, growth_rate):
+        super(DenseLayer).__init__()
+
+    
+
+
+
+
+
+
